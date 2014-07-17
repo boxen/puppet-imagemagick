bc1df728189c815b546712f37036df67b8860d06
Lucas Mazza <lucastmazza@gmail.com>
2014-07-17 17:59:14 -0300

    Remove little-cms2 since it is now an optional dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,18 +4,11 @@ class imagemagick {
   include imagemagick::config
 
   homebrew::formula {
-    'little-cms2':
-      source => 'puppet:///modules/imagemagick/brews/little-cms2.rb',
-      before => Package['boxen/brews/little-cms2'] ;
     'imagemagick':
       before => Package['boxen/brews/imagemagick'] ;
   }
 
   package {
-    'boxen/brews/little-cms2':
-      ensure  => '2.4-boxen1',
-      before  => Package['boxen/brews/imagemagick'],
-      require => Class['xquartz'] ;
     'boxen/brews/imagemagick':
       ensure  => $imagemagick::config::version,
       require => Class['xquartz'] ;
